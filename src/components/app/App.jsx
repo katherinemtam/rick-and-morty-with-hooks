8f71315632e528d36b3fb950c24fff30ae37925b
Katherine Tam <katherinemtam@gmail.com>
2021-08-03 16:50:11 -0700

    create routes for homepage and detailpage

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import DetailPage from '../../containers/DetailPage';
+import RickAndMorty from '../../containers/RickAndMorty';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Router>
+        <main>
+          <Switch>
+
+            <Route path="/" exact={true}
+              component={RickAndMorty} 
+            />
+
+            <Route path="/:id" exact={true}
+              component={DetailPage} 
+            />
+
+            <Redirect to="/" />
+            
+          </Switch>
+        </main>
+      </Router>
+    </div>
+  );
 }
