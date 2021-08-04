7ffd724942d0276dc5ba7fa7b843e0d3fefa6f38
Katherine Tam <katherinemtam@gmail.com>
2021-08-03 17:48:38 -0700

    fetchCharacter function

--- src/services/rickAndMortyApi.js
+++ src/services/rickAndMortyApi.js
@@ -1,6 +1,13 @@
 export const fetchCharacters = async () => {
   const res = await fetch('https://rickandmortyapi.com/api/character');
   const json = await res.json();
-  console.log('api', json.results);
+
   return json.results;
+};
+
+export const fetchCharacter = async (id) => {
+  const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+  const json = await res.json();
+
+  return json;
 };
