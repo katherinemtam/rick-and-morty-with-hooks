1eb37f45d94c31cd23d5d4f5a05e242a2093b307
Katherine Tam <katherinemtam@gmail.com>
2021-08-03 21:46:16 -0700

    add Link to return to homepage

--- src/components/characters/CharacterDetail.jsx
+++ src/components/characters/CharacterDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const CharacterDetail = ({ name, status, species, type, gender, origin, location, image }) => (
   <figure>
@@ -8,11 +9,12 @@ const CharacterDetail = ({ name, status, species, type, gender, origin, location
     <figcaption>
       <p>Status: {status}</p>
       <p>Species: {species}</p>
-      <p>Type: {type ? { type } : 'N/A'}</p>
+      <p>Type: {type}</p>
       <p>Gender: {gender}</p>
       <p>Origin: {origin.name}</p>
       <p>Location: {location.name}</p>
     </figcaption>
+    <Link to="/">Return Home</Link>
   </figure>
 );
 
